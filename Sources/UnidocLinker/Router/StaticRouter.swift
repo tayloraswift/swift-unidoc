c4e2df1fea84438a927e276c974c1065bda42869
taylorswift <kelvin13ma@gmail.com>
2023-06-20 22:10:59 +0000

    target hash collision diagnostics more closely to static path groups

--- Sources/UnidocLinker/Router/StaticRouter.swift
+++ Sources/UnidocLinker/Router/StaticRouter.swift
@@ -7,54 +7,38 @@ import UnidocRouting
 struct StaticRouter
 {
     private(set)
-    var hashes:[FNV24: Group<Int32>]
-    private(set)
-    var paths:[StaticPath: Group<Int32>]
+    var paths:[StaticPath: InlineDictionary<FNV24?, InlineArray<Int32>>]
 
     init()
     {
         self.paths = [:]
-        self.hashes = [:]
-    }
-}
-extension StaticRouter
-{
-    subscript(hash:FNV24) -> Group<Int32>
-    {
-        _read
-        {
-            yield  self.hashes[hash, default: []]
-        }
-        _modify
-        {
-            yield &self.hashes[hash, default: []]
-        }
     }
 }
 extension StaticRouter
 {
     subscript(namespace:ModuleIdentifier,
         path:UnqualifiedPath,
-        phylum:ScalarPhylum) -> Group<Int32>
+        phylum:ScalarPhylum) -> InlineDictionary<FNV24?, InlineArray<Int32>>
     {
         _read
         {
-            yield  self.paths[.declaration(namespace, path, phylum), default: []]
+            yield  self.paths[.declaration(namespace, path, phylum), default: [:]]
         }
         _modify
         {
-            yield &self.paths[.declaration(namespace, path, phylum), default: []]
+            yield &self.paths[.declaration(namespace, path, phylum), default: [:]]
         }
     }
-    subscript(namespace:ModuleIdentifier, name:String) -> Group<Int32>
+    subscript(namespace:ModuleIdentifier,
+        name:String) -> InlineDictionary<FNV24?, InlineArray<Int32>>
     {
         _read
         {
-            yield  self.paths[.standalone(namespace, name), default: []]
+            yield  self.paths[.standalone(namespace, name), default: [:]]
         }
         _modify
         {
-            yield &self.paths[.standalone(namespace, name), default: []]
+            yield &self.paths[.standalone(namespace, name), default: [:]]
         }
     }
 }
