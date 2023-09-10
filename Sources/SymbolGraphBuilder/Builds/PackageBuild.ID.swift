72125f601c1c714cbaac6fc395ea4c789c92b517
Diana <diana@rarestype.com>
2023-09-10 01:44:07 +0000

    lint

--- Sources/SymbolGraphBuilder/Builds/PackageBuild.ID.swift
+++ Sources/SymbolGraphBuilder/Builds/PackageBuild.ID.swift
@@ -34,34 +34,3 @@ extension PackageBuild.ID
         }
     }
 }
-extension PackageBuild.ID
-{
-    @available(*, deprecated)
-    var github:String?
-    {
-        guard case .remote(let url) = self.pin?.location,
-            let colon:String.Index = url.firstIndex(of: ":"),
-            let start:String.Index = url.index(colon, offsetBy: 3, limitedBy: url.endIndex)
-        else
-        {
-            return nil
-        }
-        switch url[..<start]
-        {
-        case "http://", "https://":
-            break
-        case _:
-            return nil
-        }
-
-        if  let end:String.Index = url.lastIndex(of: "."),
-            url[end...] == ".git"
-        {
-            return .init(url[start ..< end])
-        }
-        else
-        {
-            return .init(url[start...])
-        }
-    }
-}
