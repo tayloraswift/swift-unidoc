77154b1575e9c55d503e719ffd26cdc5774f6159
Diana <diana@rarestype.com>
2023-09-23 22:42:55 +0000

    create an API endpoint for querying the doc build status of a package

--- Sources/UnidocPages/Responses/PackageEditionsQuery.Output (ext).swift
+++ Sources/UnidocPages/Responses/PackageEditionsQuery.Output (ext).swift
@@ -1,13 +1,58 @@
 import HTTP
+import JSON
+import Media
+import UnidocDB
 import UnidocQueries
 import URI
 
 extension PackageEditionsQuery.Output:ServerResponseFactory
 {
     public
-    func response(for _:URI) throws -> ServerResponse
+    func response(as type:AcceptType?) throws -> ServerResponse
     {
-        let list:Site.Tags.List = .init(from: self)
-        return .ok(list.resource())
+        switch type
+        {
+        case .application(.json):
+            let json:JSON = .object
+            {
+                guard
+                let repo:PackageRepo = self.record.repo,
+                let release:PackageEditionsQuery.Facet = self.releases.first
+                else
+                {
+                    return
+                }
+
+                $0["repo"] = "https://\(repo.origin)"
+
+                $0["release"]
+                {
+                    $0["graphs"] = release.graphs?.count ?? 0
+                    $0["tag"] = release.edition.name
+                }
+
+                guard
+                let prerelease:PackageEditionsQuery.Facet = self.prereleases.first,
+                    prerelease.edition.patch > release.edition.patch
+                else
+                {
+                    return
+                }
+
+                $0["prerelease"]
+                {
+                    $0["graphs"] = prerelease.graphs?.count ?? 0
+                    $0["tag"] = prerelease.edition.name
+                }
+            }
+
+            return .ok(.init(
+                content: .binary(json.utf8),
+                type: .application(.json, charset: .utf8)))
+
+        case _:
+            let list:Site.Tags.List = .init(from: self)
+            return .ok(list.resource())
+        }
     }
 }
