8d0965a278064c9e86a0cfe5318147818bad2ffc
taylorswift <kelvin13ma@gmail.com>
2023-09-27 18:45:00 +0000

    rebalance UnidocServer module directories

--- Sources/UnidocServer/Server.swift
+++ Sources/UnidocServer/Server.swift
@@ -38,7 +38,7 @@ struct Server:Sendable
     let cache:Cache<Site.Asset.Get>
 
 
-    let mode:ServerMode
+    let mode:Mode
     let db:DB
 
     private
@@ -46,7 +46,7 @@ struct Server:Sendable
         port:Int,
         github:GitHubPlugin?,
         cache:Cache<Site.Asset.Get>,
-        mode:ServerMode,
+        mode:Mode,
         db:DB)
     {
         self.authority = authority
