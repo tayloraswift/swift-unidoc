0cb16556c4cb91c1dae1a5fa00ac886c981f369f
Diana <diana@rarestype.com>
2023-09-29 19:08:39 +0000

    modify UnidocBuild tool to only build prerelease docs if the semver patch is higher than the latest release

--- Sources/UnidocBuild/Main.swift
+++ Sources/UnidocBuild/Main.swift
@@ -4,6 +4,7 @@ import ModuleGraphs
 import NIOCore
 import NIOPosix
 import NIOSSL
+import SemanticVersions
 import SymbolGraphBuilder
 import SymbolGraphs
 import UnidocAutomation
@@ -48,7 +49,10 @@ enum Main
                 }
                 else if
                     let prerelease:PackageBuildStatus.Edition = package.prerelease,
-                        prerelease.graphs == 0
+                        prerelease.graphs == 0,
+                    let version:SemanticVersion = .init(refname: prerelease.tag),
+                    let release:SemanticVersion = .init(refname: package.release.tag),
+                        release.patch < version.patch
                 {
                     edition = prerelease
                 }
