6123f1df6e973a2d4179e1a82ff6dcab2185502a
taylorswift <kelvin13ma@gmail.com>
2023-11-10 04:40:10 +0000

    move SemanticVersion extension to file

--- Sources/UnidocDB/Packages/UnidocDatabase.swift
+++ Sources/UnidocDB/Packages/UnidocDatabase.swift
@@ -264,26 +264,6 @@ extension UnidocDatabase
             repo: placement.repo)
     }
 }
-
-extension SemanticVersion
-{
-    @inlinable public
-    init?(swiftRelease string:String)
-    {
-        guard
-        let i:String.Index = string.firstIndex(of: "-"),
-        let j:String.Index = string.lastIndex(of: "-"),
-            j > i,
-        let v:PatchVersion = .init(string[string.index(after: i) ..< j])
-        else
-        {
-            return nil
-        }
-
-        self = .release(v)
-    }
-}
-
 extension UnidocDatabase
 {
     public
