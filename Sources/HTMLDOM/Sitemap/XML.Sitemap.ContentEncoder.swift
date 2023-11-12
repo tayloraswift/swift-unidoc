6a99ae5eb136d37ef26e7ab93d832ab0c823248d
Diana <diana@rarestype.com>
2023-11-12 21:49:23 +0000

    found a way to factor tag emission implementations into a protocol

--- Sources/HTMLDOM/Sitemap/XML.Sitemap.ContentEncoder.swift
+++ Sources/HTMLDOM/Sitemap/XML.Sitemap.ContentEncoder.swift
@@ -14,29 +14,29 @@ extension XML.Sitemap
     }
 }
 //  These cannot be factored into protocols due to mutation of ``utf8``.
-extension XML.Sitemap.ContentEncoder
+extension XML.Sitemap.ContentEncoder:DOM.ContentEncoder
 {
-    @inlinable internal mutating
-    func emit(opening tag:some RawRepresentable<String>,
-        with yield:(inout XML.Sitemap.AttributeEncoder) -> ())
-    {
-        self.utf8.append(0x3C) // '<'
-        self.utf8.append(contentsOf: tag.rawValue.utf8)
-        yield(&self[as: XML.Sitemap.AttributeEncoder.self])
-        self.utf8.append(0x3E) // '>'
-    }
+    @usableFromInline internal
+    typealias AttributeEncoder = XML.Sitemap.AttributeEncoder
 
-    @inlinable internal mutating
-    func emit(closing tag:some RawRepresentable<String>)
+    /// Appends a *raw* UTF-8 code unit to the output stream.
+    @inlinable public mutating
+    func append(escaped codeunit:UInt8)
     {
-        self.utf8.append(0x3C) // '<'
-        self.utf8.append(0x2F) // '/'
-        self.utf8.append(contentsOf: tag.rawValue.utf8)
-        self.utf8.append(0x3E) // '>'
+        self.utf8.append(codeunit)
     }
 }
 extension XML.Sitemap.ContentEncoder
 {
+    /// Appends an *unescaped* UTF-8 code unit to the output stream.
+    /// If the code unit is one of the ASCII characters `&` `<`, or `>`,
+    /// this function replaces it with the corresponding XML entity.
+    @inlinable public mutating
+    func append(unescaped codeunit:UInt8)
+    {
+        self.utf8 += DOM.UTF8.init(codeunit)
+    }
+
     /// Writes an opening sitemap tag to the output stream.
     ///
     /// This is a low-level interface. Prefer encoding with ``subscript(_:content:)``
@@ -47,20 +47,7 @@ extension XML.Sitemap.ContentEncoder
     {
         self.emit(opening: tag, with: yield)
     }
-    /// Appends a *raw* UTF-8 code unit to the output stream.
-    @inlinable public mutating
-    func append(escaped codeunit:UInt8)
-    {
-        self.utf8.append(codeunit)
-    }
-    /// Appends an *unescaped* UTF-8 code unit to the output stream.
-    /// If the code unit is one of the ASCII characters `&` `<`, or `>`,
-    /// this function replaces it with the corresponding XML entity.
-    @inlinable public mutating
-    func append(unescaped codeunit:UInt8)
-    {
-        self.utf8 += DOM.UTF8.init(codeunit)
-    }
+
     /// Writes a closing sitemap tag to the output stream.
     ///
     /// This is a low-level interface. Prefer encoding with ``subscript(_:content:)``
