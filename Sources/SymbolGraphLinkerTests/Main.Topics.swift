3815348e37825ac86cb6aa508a2767b00b1073c9
taylorswift <kelvin13ma@gmail.com>
2023-11-17 22:42:19 +0000

    organize SymbolGraphLinker tests

---
+++ Sources/SymbolGraphLinkerTests/Main.Topics.swift
@@ -0,0 +1,169 @@
+import Testing
+
+extension Main
+{
+    struct Topics
+    {
+    }
+}
+extension Main.Topics:MarkdownTestBattery
+{
+    static
+    func run(tests:TestGroup)
+    {
+        let html:String =
+        """
+        <p>Overview overview overview</p>
+        <h2 id='Discussion'><a href='#Discussion'>Discussion</a></h2>\
+        <p>Details details details</p>
+        """
+        if  let tests:TestGroup = tests / "OneList" / "OneTopic" / "Middle"
+        {
+            Self.run(tests: tests,
+                markdown:
+                """
+                Overview overview overview
+
+                ## Topics
+
+                ### Chase Icon
+
+                -   ``StopIt``
+                -   ``DropIt``
+                -   <doc:GetAnotherTopic>
+
+                ## Discussion
+
+                Details details details
+                """,
+                expected: html,
+                topics: [3])
+        }
+        if  let tests:TestGroup = tests / "OneList" / "OneTopic" / "End"
+        {
+            Self.run(tests: tests,
+                markdown:
+                """
+                Overview overview overview
+
+                ## Discussion
+
+                Details details details
+
+                ## Topics
+
+                ### Chase Icon
+
+                -   ``StopIt``
+                -   ``DropIt``
+                -   <doc:GetAnotherTopic>
+                """,
+                expected: html,
+                topics: [3])
+        }
+        if  let tests:TestGroup = tests / "OneList" / "ManyTopics"
+        {
+            Self.run(tests: tests,
+                markdown:
+                """
+                Overview overview overview
+
+                ## Discussion
+
+                Details details details
+
+                ## Topics
+
+                ### Chase Icon
+
+                -   ``StopIt``
+                -   ``DropIt``
+                -   <doc:GetAnotherTopic>
+
+                ### Taylor Swift
+
+                -   ``WeAreNeverEverGettingBackTogether``
+                -   ``AllTooWell``
+
+                """,
+                expected: html,
+                topics: [3, 2])
+        }
+        if  let tests:TestGroup = tests / "ManyLists"
+        {
+            Self.run(tests: tests,
+                markdown:
+                """
+                Overview overview overview
+
+                ## Topics
+
+                ### Dua Lipa
+
+                -   ``DanceTheNight``
+
+                ## Discussion
+
+                Details details details
+
+                ## Topics
+
+                ### Chase Icon
+
+                -   ``StopIt``
+                -   ``DropIt``
+                -   <doc:GetAnotherTopic>
+
+                ### Taylor Swift
+
+                -   ``WeAreNeverEverGettingBackTogether``
+                -   ``AllTooWell``
+
+                """,
+                expected: html,
+                topics: [1, 3, 2])
+        }
+        if  let tests:TestGroup = tests / "ManyLists" / "Discussions"
+        {
+            Self.run(tests: tests,
+                markdown:
+                """
+                Overview overview overview
+
+                ## Topics
+
+                ### Dua Lipa
+
+                Selected songs by Dua Lipa.
+
+                -   ``DanceTheNight``
+
+                ## Discussion
+
+                Details details details
+
+                ## Topics
+
+                ### Chase Icon
+
+                Lyrics from *Like Me*.
+
+                -   ``StopIt``
+                -   ``DropIt``
+                -   <doc:GetAnotherTopic>
+
+                ### Taylor Swift
+
+                Selected songs by Taylor Swift.
+
+                >   Note: These are from the original Red album.
+
+                -   ``WeAreNeverEverGettingBackTogether``
+                -   ``AllTooWell``
+
+                """,
+                expected: html,
+                topics: [1, 3, 2])
+        }
+    }
+}
---
+++ Sources/SymbolGraphLinkerTests/MarkdownTestBattery.swift
@@ -0,0 +1,57 @@
+import HTML
+import MarkdownABI
+import MarkdownAST
+import MarkdownParsing
+import MarkdownSemantics
+import Testing
+import UnidocDiagnostics
+
+@_spi(testable)
+import SymbolGraphLinker
+
+protocol MarkdownTestBattery:TestBattery
+{
+}
+extension MarkdownTestBattery
+{
+    static
+    func run(tests:TestGroup, markdown:MarkdownSource, expected:String, topics:[Int] = [])
+    {
+        let parser:SwiftFlavoredMarkdownParser<SwiftFlavoredMarkdownComment> = .init()
+        var ignore:DiagnosticContext<StaticSymbolicator> = .init()
+
+        tests.do
+        {
+            let documentation:MarkdownDocumentation = markdown.parse(using: parser,
+                with: &ignore)
+            let overview:MarkdownBinary? = documentation.overview.map
+            {
+                .init(bytecode: .init(with: $0.emit(into:)))
+            }
+            let details:MarkdownBinary = .init(bytecode: .init
+            {
+                (encoder:inout MarkdownBinaryEncoder)in
+
+                documentation.details.visit
+                {
+                    $0.emit(into: &encoder)
+                }
+            })
+            let html:HTML = try .init
+            {
+                try overview?.render(to: &$0)
+
+                $0.append(escaped: 0x0A) // '\n'
+
+                try details.render(to: &$0)
+            }
+
+            tests.expect(html.description ==? expected)
+
+            if  let tests:TestGroup = tests / "Topics"
+            {
+                tests.expect(documentation.topics.map(\.members.count) ..? topics)
+            }
+        }
+    }
+}
