7feb6eb9248a8743e324ad1926369426c522590b
Diana <diana@rarestype.com>
2023-11-24 23:27:55 +0000

    code format

---
+++ Sources/UnidocLinker/Resolver/DynamicResolver.Autolink.swift
@@ -0,0 +1,55 @@
+import Sources
+import SymbolGraphs
+import Unidoc
+import UnidocDiagnostics
+
+extension DynamicResolver
+{
+    struct Autolink
+    {
+        let unresolved:SymbolGraph.Outline.Unresolved
+        let location:SourceLocation<Unidoc.Scalar>?
+
+        init(_ unresolved:SymbolGraph.Outline.Unresolved,
+            location:SourceLocation<Unidoc.Scalar>?)
+        {
+            self.unresolved = unresolved
+            self.location = location
+        }
+    }
+}
+extension DynamicResolver.Autolink:DiagnosticSubject
+{
+    var context:SourceContext { .init() }
+}
+extension DynamicResolver.Autolink
+{
+    var parsed:Codelink?
+    {
+        switch self.unresolved.type
+        {
+        case .doc:
+            guard
+            let doclink:Doclink = .init(unresolved.link)
+            else
+            {
+                return nil
+            }
+
+            return .init(doclink.path.joined(separator: "/"))
+
+        case .ucf:
+            return .init(unresolved.link)
+
+        case .unidocV3:
+            guard
+            let unidocV3:CodelinkV3 = .init(unresolved.link)
+            else
+            {
+                return nil
+            }
+
+            return .init(v3: unidocV3)
+        }
+    }
+}
