95b588c433a01ebeda1ae0e836069f9070a3b710
taylorswift <kelvin13ma@gmail.com>
2023-12-08 23:55:24 +0000

    fix #86

--- Sources/SymbolGraphCompilerTests/Main.DefaultImplementations.swift
+++ Sources/SymbolGraphCompilerTests/Main.DefaultImplementations.swift
@@ -23,22 +23,39 @@ extension Main.DefaultImplementations:CompilerTestBattery
         namespaces:[[Compiler.Namespace]],
         extensions:[Compiler.Extension])
     {
+        var features:[Symbol.Decl: [Symbol.Decl]] = [:]
         for namespace:Compiler.Namespace in namespaces.joined()
         {
             for decl:Compiler.Decl in namespace.decls
             {
-                switch decl.id
-                {
-                case "s22DefaultImplementations4EnumO":
-                    tests.expect(decl.features ..?
-                    [
-                        "s22DefaultImplementations9ProtocolBPAAE1fyyF",
-                    ])
-
-                case _:
-                    continue
-                }
+                features[decl.id, default: []] += decl.features
             }
+        }
+
+        if  let tests:TestGroup = tests / "DefaultImplementationInheritance",
+            let features:[Symbol.Decl] = tests.expect(
+                value: features["s22DefaultImplementations4EnumO"])
+        {
+            tests.expect(features **?
+            [
+                "s22DefaultImplementations9ProtocolBPAAE1fyyF",
+                "s22DefaultImplementations9ProtocolCPAAE2idSSvp",
+            ])
+        }
+
+        let nested:[Symbol.Decl: [Symbol.Decl]] = extensions.reduce(into: [:])
+        {
+            $0[$1.signature.extended.type, default: []] += $1.nested
+        }
+
+        if  let tests:TestGroup = tests / "DefaultImplementationScopes",
+            let nested:[Symbol.Decl] = tests.expect(
+                value: nested["s22DefaultImplementations9ProtocolBP"])
+        {
+            tests.expect(nested **?
+            [
+                "s22DefaultImplementations9ProtocolBPAAE1fyyF",
+            ])
         }
     }
 }
