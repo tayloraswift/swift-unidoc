d504090651d6f83a17977669b7a3c5f920116769
Diana <diana@rarestype.com>
2023-12-09 22:43:02 +0000

    fix compile failure

--- Sources/SymbolGraphParts/Availability/Availability.VersionRange (ext).swift
+++ Sources/SymbolGraphParts/Availability/Availability.VersionRange (ext).swift
@@ -8,7 +8,7 @@ extension Availability.VersionRange:JSONObjectDecodable
     init(json:JSON.ObjectDecoder<PatchVersion.CodingKey>) throws
     {
         //  This allows us to model extremely high version numbers
-        if  let major:UInt16 = .init(exactly: try json[.major].decode(to: UInt.self))
+        if  let major:Int16 = .init(exactly: try json[.major].decode(to: UInt.self))
         {
             self = .since(try .init(major,
                 try json[.minor]?.decode(),
