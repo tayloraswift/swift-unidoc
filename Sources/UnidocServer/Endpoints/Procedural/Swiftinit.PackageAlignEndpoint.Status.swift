8dc1cb4b5288976990286f37428bd2487976a624
taylorswift <kelvin13ma@gmail.com>
2023-12-15 01:25:48 +0000

    implement UI for transferring packaged between realms

--- Sources/UnidocServer/Endpoints/Procedural/Swiftinit.PackageAlignEndpoint.Status.swift
+++ Sources/UnidocServer/Endpoints/Procedural/Swiftinit.PackageAlignEndpoint.Status.swift
@@ -7,7 +7,7 @@ extension Swiftinit.PackageAlignEndpoint
 {
     enum Status
     {
-        case align(Unidoc.Package, to:Unidoc.Realm?)
+        case align(Unidoc.PackageMetadata, to:Unidoc.Realm?)
         case noSuchPackage
         case noSuchRealm
     }
@@ -18,9 +18,13 @@ extension Swiftinit.PackageAlignEndpoint.Status:HTTP.ServerResponseFactory
     {
         switch self
         {
-        case .align:            .ok("Operation in progress")
-        case .noSuchPackage:    .notFound("No such package")
-        case .noSuchRealm:      .notFound("No such realm")
+        case .align(let package, to: _):
+            .redirect(.see(other: "\(Swiftinit.Tags[package.symbol])"))
+
+        case .noSuchPackage:
+            .notFound("No such package")
+        case .noSuchRealm:
+            .notFound("No such realm")
         }
     }
 }
