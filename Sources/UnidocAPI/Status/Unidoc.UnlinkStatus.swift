354039eb04e8a28d3b81432f4b06f97b830a5de7
Diana <diana@rarestype.com>
2023-12-15 03:39:32 +0000

    don’t replace release volumes with prerelease volumes when performing a full rebuild

---
+++ Sources/UnidocAPI/Status/Unidoc.UnlinkStatus.swift
@@ -0,0 +1,13 @@
+import Unidoc
+
+extension Unidoc
+{
+    @frozen public
+    enum UnlinkStatus:Equatable, Sendable
+    {
+        /// The unlink request was declined, most likely because the volume is a release.
+        case declined(Unidoc.Edition)
+        /// The unlink request was successful.
+        case unlinked(Unidoc.Edition)
+    }
+}
