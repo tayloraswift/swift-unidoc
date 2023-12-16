c4cf9ef867840ad4fd21929ae403931701487e69
taylorswift <kelvin13ma@gmail.com>
2023-12-16 22:27:28 +0000

    document UnidocDatabase.package(named:with:) and realm(named:with:)

--- Sources/UnidocQueries/UnidocDatabase (ext).swift
+++ Sources/UnidocQueries/UnidocDatabase (ext).swift
@@ -5,6 +5,8 @@ import UnidocRecords
 
 extension UnidocDatabase
 {
+    /// Load the metadata for a package by name. The returned package might have a different
+    /// canonical name than the one provided.
     public
     func package(named symbol:Symbol.Package,
         with session:Mongo.Session) async throws -> Unidoc.PackageMetadata?
@@ -13,6 +15,8 @@ extension UnidocDatabase
             query: Unidoc.AliasResolutionQuery<PackageAliases, Packages>.init(symbol: symbol),
             with: session)
     }
+    /// Load the metadata for a realm by name. The returned realm might have a different
+    /// canonical name than the one provided.
     public
     func realm(named symbol:String,
         with session:Mongo.Session) async throws -> Unidoc.RealmMetadata?
