2b7649e91d3a1f2cb0f5a06b0fa7b473ef8a6fb3
Diana <diana@rarestype.com>
2023-12-17 22:48:15 +0000

    adjust language inference logic to fix #96

--- Sources/SymbolGraphs/Modules/SymbolGraph.ModuleLanguage.swift
+++ Sources/SymbolGraphs/Modules/SymbolGraph.ModuleLanguage.swift
@@ -22,14 +22,14 @@ extension SymbolGraph.ModuleLanguage
     /// This is most useful when inferring the language of a module from the languages of its
     /// constituent source files. For example, a C++ module may contain `.c` or `.h` files.
     @inlinable public static
-    func | (lhs:Self, rhs:Self) -> Self
+    func | (lhs:Self?, rhs:Self) -> Self
     {
-        max(lhs, rhs)
+        lhs.map { max($0, rhs) } ?? rhs
     }
 
     /// See ``|(_:_:)``.
     @inlinable public static
-    func |= (lhs:inout Self, rhs:Self)
+    func |= (lhs:inout Self?, rhs:Self)
     {
         lhs = lhs | rhs
     }
