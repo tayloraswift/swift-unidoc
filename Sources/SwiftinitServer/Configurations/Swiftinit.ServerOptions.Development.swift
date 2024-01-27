3749be8f93062d45f453ad547194c35df3c8e14a
taylorswift <kelvin13ma@gmail.com>
2024-01-27 02:00:07 +0000

    add warning about publically writable buckets

--- Sources/SwiftinitServer/Configurations/Swiftinit.ServerOptions.Development.swift
+++ Sources/SwiftinitServer/Configurations/Swiftinit.ServerOptions.Development.swift
@@ -10,6 +10,9 @@ extension Swiftinit.ServerOptions
         /// Whether to enable IP whitelisting.
         var whitelists:Bool
 
+        /// A test bucket for development. For this to work, you should probably make the bucket
+        /// publically writable. It goes without saying that you should delete such a bucket
+        /// as soon as you are done testing it.
         var bucket:AWS.S3.Bucket?
 
         var port:Int
