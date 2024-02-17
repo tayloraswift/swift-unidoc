1b63356e0ac60ff720ddba8825b0b40b42b0ad3f
taylorswift <kelvin13ma@gmail.com>
2024-02-17 01:35:19 +0000

    relax children requirements for Markdown.BlockArticle

--- Sources/MarkdownSemantics/AST/Tutorials/Markdown.BlockArticle.StructuralError.swift
+++ Sources/MarkdownSemantics/AST/Tutorials/Markdown.BlockArticle.StructuralError.swift
@@ -3,6 +3,5 @@ extension Markdown.BlockArticle
     enum StructuralError:Error
     {
         case intro(type:Markdown.BlockElement.Type)
-        case child(type:Markdown.BlockElement.Type)
     }
 }
