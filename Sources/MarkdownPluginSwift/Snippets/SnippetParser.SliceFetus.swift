09990a4c843952d9bcec6797e404d00e7baf18d7
Diana <diana@rarestype.com>
2024-02-17 22:41:34 +0000

    fix #134, add a regression test

--- Sources/MarkdownPluginSwift/Snippets/SnippetParser.SliceFetus.swift
+++ Sources/MarkdownPluginSwift/Snippets/SnippetParser.SliceFetus.swift
@@ -10,7 +10,8 @@ extension SnippetParser
         private
         var start:AbsolutePosition?
 
-        init(id:String, position:AbsolutePosition, marker:(line:Int, indent:Int) = (1, 0))
+        private
+        init(id:String, position:AbsolutePosition, marker:(line:Int, indent:Int))
         {
             self.slice = .init(id: id, marker: marker)
             self.start = position
@@ -19,12 +20,33 @@ extension SnippetParser
 }
 extension SnippetParser.SliceFetus
 {
-    mutating
-    func show(at position:AbsolutePosition)
+    static
+    func anonymous(start position:AbsolutePosition) -> Self
     {
+        .init(id: "", position: position, marker: (1, 0))
+    }
+
+    static
+    func named(id:String, marker:SnippetParser.SliceMarker) -> Self
+    {
+        .init(id: id,
+            position: marker.after,
+            marker: (line: marker.line, indent: marker.indent))
+    }
+}
+extension SnippetParser.SliceFetus
+{
+    mutating
+    func show(with marker:SnippetParser.SliceMarker)
+    {
+        //  This is only effective for the anonymous slice.
+        if  self.slice.ranges.isEmpty
+        {
+            self.slice.marker = (line: marker.line, indent: marker.indent)
+        }
         if  case nil = self.start
         {
-            self.start = position
+            self.start = marker.after
         }
         else
         {
@@ -42,6 +64,10 @@ extension SnippetParser.SliceFetus
             //  TODO: Emit a warning.
             return
         }
+        defer
+        {
+            self.start = nil
+        }
         //  Two ways this check can fail:
         //
         //  1.  Something resembling a control comment appears in the snippet abstract.
@@ -49,7 +75,6 @@ extension SnippetParser.SliceFetus
         if  start < position
         {
             self.slice.ranges.append(start ..< position)
-            self.start = nil
         }
     }
 
