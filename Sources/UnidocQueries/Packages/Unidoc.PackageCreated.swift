c16e8ca8e9a73f1581efa3ce350cc6e5ae549277
Diana <diana@rarestype.com>
2024-02-23 20:47:15 +0000

    add integration tests for the external link sanitizer

--- Sources/UnidocQueries/Packages/Unidoc.PackageCreated.swift
+++ Sources/UnidocQueries/Packages/Unidoc.PackageCreated.swift
@@ -5,6 +5,44 @@ import UnixTime
 
 extension Unidoc
 {
+    /// A predicate that matches all packages whose associated GitHub repositories are owned by
+    /// a particular user or organization.
+    @frozen public
+    struct PackageOwner:Equatable, Hashable, Sendable
+    {
+        @usableFromInline
+        let owner:String
+        @usableFromInline
+        let limit:Int
+
+        @inlinable
+        init(by owner:String, limit:Int)
+        {
+            self.owner = owner
+            self.limit = limit
+        }
+    }
+}
+extension Unidoc.PackageOwner:Unidoc.PackagePredicate
+{
+    @inlinable public
+    var hint:Mongo.CollectionIndex? { Unidoc.DB.Packages.indexAccount }
+
+    public
+    func extend(pipeline:inout Mongo.PipelineEncoder)
+    {
+        pipeline[stage: .match] = .init
+        {
+            $0[Unidoc.PackageMetadata[.repo]] { $0[.exists] = true }
+            $0[Unidoc.PackageMetadata[.repo] / Unidoc.PackageRepo[.account]] = self.owner
+        }
+
+        pipeline[stage: .limit] = self.limit
+    }
+}
+
+extension Unidoc
+{
     /// A predicate that matches all packages whose associated GitHub repositories were created
     /// during a specific timeframe.
     ///
