09edd39f088b804644e90aa965ad722254b928b6
Diana <diana@rarestype.com>
2024-02-28 20:27:02 +0000

    increment symbol graph ABI version

--- Sources/SymbolGraphs/SymbolGraphABI.swift
+++ Sources/SymbolGraphs/SymbolGraphABI.swift
@@ -3,5 +3,5 @@ import SemanticVersions
 @frozen public
 enum SymbolGraphABI
 {
-    @inlinable public static var version:PatchVersion { .v(0, 8, 22) }
+    @inlinable public static var version:PatchVersion { .v(0, 8, 23) }
 }
