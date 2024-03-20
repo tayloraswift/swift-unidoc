ef65dd85b49663bf7d8b0b7a59b1644ac9e637dd
Diana <diana@rarestype.com>
2024-03-20 20:02:12 +0000

    adopt nested protocols in DOM module

--- Sources/DOM/DOM.Attribute.swift
+++ Sources/DOM/DOM.Attribute.swift
@@ -1,14 +1,10 @@
 extension DOM
 {
     @usableFromInline
-    typealias Attribute = _DOMAttribute
-}
-
-/// The name of this protocol is ``DOM.Attribute``.
-@usableFromInline
-protocol _DOMAttribute
-{
-    var name:String { get }
+    protocol Attribute
+    {
+        var name:String { get }
+    }
 }
 extension DOM.Attribute where Self:RawRepresentable<String>
 {
