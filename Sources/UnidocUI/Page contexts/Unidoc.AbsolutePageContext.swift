04fbd3aab74d5653554dff8bf6c8cdcfc84c36f9
Diana <diana@rarestype.com>
2024-05-06 18:51:42 +0000

    we should apply the same URL formatting logic to the faux existential AnyVertex

--- Sources/UnidocUI/Page contexts/Unidoc.AbsolutePageContext.swift
+++ Sources/UnidocUI/Page contexts/Unidoc.AbsolutePageContext.swift
@@ -10,13 +10,21 @@ extension Unidoc
         public override
         subscript(vertex id:Unidoc.Scalar) -> Unidoc.LinkReference<Unidoc.AnyVertex>?
         {
-            super[vertex: id]?.map { "https://swiftinit.org\($0)" }
-        }
+            guard
+            let link:Unidoc.LinkReference<Unidoc.AnyVertex> = super[vertex: id]
+            else
+            {
+                return nil
+            }
 
-        public override
-        subscript(culture id:Unidoc.Scalar) -> Unidoc.LinkReference<Unidoc.CultureVertex>?
-        {
-            super[culture: id]?.map { "https://swiftinit.org\($0)" }
+            if  case .article(let article) = link.vertex
+            {
+                return link.map { self.rewrite($0, to: article) }
+            }
+            else
+            {
+                return link.map(Self.rewrite(_:))
+            }
         }
 
         public override
@@ -29,27 +37,45 @@ extension Unidoc
                 return nil
             }
 
-            return link.map
-            {
-                if  self.volume.id == id.edition
-                {
-                    //  This is a link to an article in the same volume. Most likely, the API
-                    //  user wants to also host the other article under the same domain. Because
-                    //  we know article paths are at most one component deep, we can just return
-                    //  the last component of other article’s path as a relative URI.
-                    return "../\(URI.Path.Component.push(link.vertex.stem.last.lowercased()))"
-                }
-                else
-                {
-                    return "https://swiftinit.org\($0)"
-                }
-            }
+            return link.map { self.rewrite($0, to: link.vertex) }
+        }
+
+        public override
+        subscript(culture id:Unidoc.Scalar) -> Unidoc.LinkReference<Unidoc.CultureVertex>?
+        {
+            super[culture: id]?.map(Self.rewrite(_:))
         }
 
         public override
         subscript(decl id:Unidoc.Scalar) -> Unidoc.LinkReference<Unidoc.DeclVertex>?
         {
-            super[decl: id]?.map { "https://swiftinit.org\($0)" }
+            super[decl: id]?.map(Self.rewrite(_:))
+        }
+    }
+}
+extension Unidoc.AbsolutePageContext
+{
+    @inline(__always)
+    private static
+    func rewrite(_ uri:String) -> String
+    {
+        "https://swiftinit.org\(uri)"
+    }
+
+    private
+    func rewrite(_ uri:String, to article:Unidoc.ArticleVertex) -> String
+    {
+        if  self.volume.id == article.id.edition
+        {
+            //  This is a link to an article in the same volume. Most likely, the API
+            //  user wants to also host the other article under the same domain. Because
+            //  we know article paths are at most one component deep, we can just return
+            //  the last component of the other article’s path as a relative URI.
+            return "../\(URI.Path.Component.push(article.stem.last.lowercased()))"
+        }
+        else
+        {
+            return Self.rewrite(uri)
         }
     }
 }
