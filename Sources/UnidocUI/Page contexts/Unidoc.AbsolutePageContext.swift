7f9b9464899c4a01d2c40ee9a0d762202533f832
taylorswift <mdianna1989@gmail.com>
2024-05-06 18:31:39 +0000

    implement #205

--- Sources/UnidocUI/Page contexts/Unidoc.AbsolutePageContext.swift
+++ Sources/UnidocUI/Page contexts/Unidoc.AbsolutePageContext.swift
@@ -1,5 +1,6 @@
 import UnidocRender
 import UnidocRecords
+import URI
 
 extension Unidoc
 {
@@ -21,7 +22,28 @@ extension Unidoc
         public override
         subscript(article id:Unidoc.Scalar) -> Unidoc.LinkReference<Unidoc.ArticleVertex>?
         {
-            super[article: id]?.map { "https://swiftinit.org\($0)" }
+            guard
+            let link:Unidoc.LinkReference<Unidoc.ArticleVertex> = super[article: id]
+            else
+            {
+                return nil
+            }
+
+            return link.map
+            {
+                if  self.volume.id == id.edition
+                {
+                    //  This is a link to an article in the same volume. Most likely, the API
+                    //  user wants to also host the other article under the same domain. Because
+                    //  we know article paths are at most one component deep, we can just return
+                    //  the last component of other article’s path as a relative URI.
+                    return "../\(URI.Path.Component.push(link.vertex.stem.last.lowercased()))"
+                }
+                else
+                {
+                    return "https://swiftinit.org\($0)"
+                }
+            }
         }
 
         public override
