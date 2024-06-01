df9083e2bace89c12ec9130ae3d12aaa7248883e
Diana <diana@rarestype.com>
2024-06-01 22:12:27 +0000

    expel MD5, InlineBuffer, and SHA1 modules to the swift-hash repo

--- Sources/MD5/exports.swift
+++
@@ -1,4 +0,0 @@
-/// Much of this module’s functionality is `@inlinable` and would require users of ``MD5`` to
-/// also import ``InlineBuffer`` along with it.  However, ``InlineBuffer`` is an implementation
-/// detail of ``MD5`` and clients should not interact with it directly.
-@_exported import struct InlineBuffer.InlineBuffer
