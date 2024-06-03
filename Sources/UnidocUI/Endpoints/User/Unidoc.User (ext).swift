297c21c342822f4f7ac6a50df9abbebfcd529765
Diana <diana@rarestype.com>
2024-06-03 21:05:37 +0000

    compute github avatar url ourselves, to optimize bandwidth usage by requesting smaller images

--- Sources/UnidocUI/Endpoints/User/Unidoc.User (ext).swift
+++ Sources/UnidocUI/Endpoints/User/Unidoc.User (ext).swift
@@ -9,9 +9,23 @@ extension Unidoc.User
     private
     func card<Tools>(some tools:Tools) -> Unidoc.UserCard<Tools>
     {
-        .init(id: self.id,
+        let icon:String?
+        //  GitHub discourages us from trying to predict the user’s avatar URL. However, the
+        //  URL they provide us by default serves an image that is far too large for our
+        //  purposes. We can modify the URL to request a smaller image, but at that point we
+        //  might as well just compute the whole URL ourselves.
+        if  let id:UInt32 = self.id.github
+        {
+            icon = "https://avatars.githubusercontent.com/u/\(id)?s=64"
+        }
+        else
+        {
+            icon = nil
+        }
+
+        return .init(id: self.id,
             symbol: self.symbol ?? self.github?.login,
-            icon: self.github?.icon,
+            icon: icon,
             tools: tools)
     }
 }
