05af95dd9fa1391ffa2745eaed301b8f93ce0513
taylorswift <mdianna1989@gmail.com>
2024-06-18 21:07:52 +0000

    work around two occurrences of https://github.com/apple/swift/issues/70133

--- Sources/UnidocServer/Operations/Interactive/Unidoc.InteractiveOperation.swift
+++ Sources/UnidocServer/Operations/Interactive/Unidoc.InteractiveOperation.swift
@@ -6,7 +6,9 @@ extension Unidoc
     public
     protocol InteractiveOperation:Sendable
     {
-        consuming
+        /// DO NOT REPLACE `__consuming` with `consuming`, it will be miscompiled due to
+        /// https://github.com/apple/swift/issues/70133
+        __consuming
         func load(from server:borrowing Server,
             with state:UserSessionState,
             as format:RenderFormat) async throws -> HTTP.ServerResponse?
