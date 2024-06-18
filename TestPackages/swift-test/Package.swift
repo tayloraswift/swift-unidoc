d17e8a4d79bad07b6b075d17aadd73250e6b0184
taylorswift <mdianna1989@gmail.com>
2024-06-18 22:56:45 +0000

    add DocC compatibility tests

--- TestPackages/swift-test/Package.swift
+++ TestPackages/swift-test/Package.swift
@@ -4,9 +4,11 @@ import PackageDescription
 let package:Package = .init(name: "Swift Unidoc Test Modules",
     products:
     [
+        .library(name: "DocCOptions", targets: ["DocCOptions"]),
         .library(name: "LinkAnchors", targets: ["LinkAnchors"]),
     ],
     targets:
     [
+        .target(name: "DocCOptions"),
         .target(name: "LinkAnchors"),
     ])
