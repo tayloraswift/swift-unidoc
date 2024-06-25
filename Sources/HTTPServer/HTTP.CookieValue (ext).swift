050bc66f69ead7b83cd461e5b5604e869eb213e6
Diana <diana@rarestype.com>
2024-06-25 21:42:02 +0000

    make session cookie last for up to 90d

---
+++ Sources/HTTPServer/HTTP.CookieValue (ext).swift
@@ -0,0 +1,10 @@
+extension HTTP.CookieValue
+{
+    @inlinable public
+    init(_ value:String, _ configure:(inout HTTP.CookieEncoder) throws -> Void) rethrows
+    {
+        var encoder:HTTP.CookieEncoder = .init(string: value)
+        try configure(&encoder)
+        self.init(encoder.string)
+    }
+}
