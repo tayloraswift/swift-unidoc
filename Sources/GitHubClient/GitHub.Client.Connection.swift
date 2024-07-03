fab89c908089f08c2d08db32f52c800e48fa028c
taylorswift <mdianna1989@gmail.com>
2024-07-03 23:59:11 +0000

    add POST methods to GitHub.Client.Connection, and start specifying the x-github-api-version

--- Sources/GitHubClient/GitHub.Client.Connection.swift
+++ Sources/GitHubClient/GitHub.Client.Connection.swift
@@ -78,21 +78,78 @@ extension GitHub.Client<GitHub.PersonalAccessToken>.Connection
         }
     }
 }
-extension GitHub.Client<GitHub.OAuth>.Connection
-{
-    /// Run a REST API request.
-    @inlinable public
-    func get<Response>(_:Response.Type = Response.self,
-        from endpoint:String) async throws -> Response where Response:JSONDecodable
-    {
-        try await self.get(from: endpoint, with: .basic(self.app))
-    }
-}
 extension GitHub.Client.Connection
 {
-    /// Run a REST API request with the given credentials.
+    @discardableResult
     @inlinable public
-    func get<Response>(_:Response.Type = Response.self,
+    func post(to endpoint:String,
+        with authorization:GitHub.ClientAuthorization) async throws -> UInt
+    {
+        try await self.post(expecting: Never.self, from: endpoint, with: authorization).0
+    }
+
+    @inlinable public
+    func post<Response>(expecting _:Response.Type = Response.self,
+        from endpoint:String,
+        with authorization:GitHub.ClientAuthorization) async throws -> Response
+        where Response:JSONDecodable
+    {
+        let (status, response):(UInt, Response?) = try await self.post(expecting: Response.self,
+            from: endpoint,
+            with: authorization)
+
+        if  let response:Response
+        {
+            return response
+        }
+        else
+        {
+            throw HTTP.StatusError.init(code: status)
+        }
+    }
+
+    @inlinable
+    func post<Response>(expecting _:Response.Type,
+        from endpoint:String,
+        with authorization:GitHub.ClientAuthorization) async throws -> (UInt, Response?)
+        where Response:JSONDecodable
+    {
+        let response:HTTP.Client2.Facet = try await self.http2.fetch([
+            ":method": "POST",
+            ":scheme": "https",
+            ":authority": self.http2.remote,
+            ":path": endpoint,
+
+            "authorization": authorization.header,
+            //  GitHub will reject the API request if the user-agent is not set.
+            "user-agent": self.agent,
+            "accept": "application/vnd.github+json",
+            "x-github-api-version": "2022-11-28",
+        ])
+
+        guard
+        let status:UInt = response.status,
+        case 200 ... 299 = status
+        else
+        {
+            throw HTTP.StatusError.init(code: response.status,
+                message: String.init(decoding: response.body, as: Unicode.UTF8.self))
+        }
+
+        if  response.body.isEmpty
+        {
+            return (status, nil)
+        }
+        else
+        {
+            let json:JSON = .init(utf8: response.body[...])
+            return (status, try json.decode())
+        }
+    }
+
+    /// Run a REST API request with the given credentials, following up to one redirect.
+    @inlinable public
+    func get<Response>(expecting _:Response.Type = Response.self,
         from endpoint:String,
         with authorization:GitHub.ClientAuthorization) async throws -> Response
         where Response:JSONDecodable
@@ -149,5 +206,15 @@ extension GitHub.Client.Connection
         }
 
         throw HTTP.StatusError.init(code: status)
+    }
+}
+extension GitHub.Client<GitHub.OAuth>.Connection
+{
+    /// Run a REST API request.
+    @inlinable public
+    func get<Response>(expecting _:Response.Type = Response.self,
+        from endpoint:String) async throws -> Response where Response:JSONDecodable
+    {
+        try await self.get(from: endpoint, with: .basic(self.app))
     }
 }
