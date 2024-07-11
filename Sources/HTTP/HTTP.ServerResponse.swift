e19024dfd1ebef3de7a483ae6fabef8f6fd76b0c
Diana <diana@rarestype.com>
2024-07-11 21:03:56 +0000

    add HTTP response helpers

--- Sources/HTTP/HTTP.ServerResponse.swift
+++ Sources/HTTP/HTTP.ServerResponse.swift
@@ -15,56 +15,62 @@ extension HTTP
 }
 extension HTTP.ServerResponse
 {
-    @inlinable public static
-    func ok(_ resource:HTTP.Resource) -> Self
+    @inlinable public
+    static func ok(_ resource:HTTP.Resource) -> Self
     {
         .resource(resource, status: 200)
     }
 
-    @inlinable public static
-    var noContent:Self
+    @inlinable public
+    static func created(_ resource:HTTP.Resource) -> Self
+    {
+        .resource(resource, status: 201)
+    }
+
+    @inlinable public
+    static var noContent:Self
     {
         .resource("", status: 204)
     }
 
-    @inlinable public static
-    func multiple(_ resource:HTTP.Resource) -> Self
+    @inlinable public
+    static func multiple(_ resource:HTTP.Resource) -> Self
     {
         .resource(resource, status: 300)
     }
 
-    @inlinable public static
-    func unauthorized(_ resource:HTTP.Resource) -> Self
+    @inlinable public
+    static func unauthorized(_ resource:HTTP.Resource) -> Self
     {
         .resource(resource, status: 401)
     }
 
-    @inlinable public static
-    func forbidden(_ resource:HTTP.Resource) -> Self
+    @inlinable public
+    static func forbidden(_ resource:HTTP.Resource) -> Self
     {
         .resource(resource, status: 403)
     }
 
-    @inlinable public static
-    func notFound(_ resource:HTTP.Resource) -> Self
+    @inlinable public
+    static func notFound(_ resource:HTTP.Resource) -> Self
     {
         .resource(resource, status: 404)
     }
 
-    @inlinable public static
-    func gone(_ resource:HTTP.Resource) -> Self
+    @inlinable public
+    static func gone(_ resource:HTTP.Resource) -> Self
     {
         .resource(resource, status: 410)
     }
 
-    @inlinable public static
-    func unsupportedMediaType(_ resource:HTTP.Resource) -> Self
+    @inlinable public
+    static func unsupportedMediaType(_ resource:HTTP.Resource) -> Self
     {
         .resource(resource, status: 415)
     }
 
-    @inlinable public static
-    func error(_ resource:HTTP.Resource) -> Self
+    @inlinable public
+    static func error(_ resource:HTTP.Resource) -> Self
     {
         .resource(resource, status: 500)
     }
