f9836a97b5687a8bafd690982573f61223743657
taylorswift <mdianna1989@gmail.com>
2024-07-17 22:43:23 +0000

    combine the --swift-runtime and --swift-path options into a single --swift-toolchain path

---
+++ Sources/UnidocClient/Unidoc.Toolchain.swift
@@ -0,0 +1,21 @@
+import SymbolGraphBuilder
+import System
+
+extension Unidoc
+{
+    @frozen public
+    struct Toolchain:Equatable, Sendable
+    {
+        public
+        let usr:FilePath.Directory?
+        public
+        let sdk:SSGC.AppleSDK?
+
+        @inlinable public
+        init(usr:FilePath.Directory?, sdk:SSGC.AppleSDK? = nil)
+        {
+            self.usr = usr
+            self.sdk = sdk
+        }
+    }
+}
