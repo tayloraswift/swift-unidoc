7d0b1e772b30f3e68cad6f34082f1d4bb7e2b0cb
Dianna <mdianna1989@gmail.com>
2024-08-28 01:18:03 +0000

    add a tool for automatically configuring MongoDB, and update Quickstart guide

--- Sources/unidoc-tools/Main.swift
+++ Sources/unidoc-tools/Main.swift
@@ -6,6 +6,7 @@ struct Main:AsyncParsableCommand
 {
     static let configuration:CommandConfiguration = .init(subcommands: [
             SSGC.Compile.self,
+            Init.self,
             Local.self,
             Preview.self
         ])
