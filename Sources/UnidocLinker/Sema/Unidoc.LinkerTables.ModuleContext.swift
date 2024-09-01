2bc0178bc9ed03a51c3bb8943d8dd1cf6750e742
Diana <diana@rarestype.com>
2024-09-01 00:01:25 +0000

    write a new, lightweight dynamic linker optimized for newer symbol graphs

---
+++ Sources/UnidocLinker/Sema/Unidoc.LinkerTables.ModuleContext.swift
@@ -0,0 +1,13 @@
+import SymbolGraphs
+import Symbols
+import Unidoc
+
+extension Unidoc.LinkerTables
+{
+    struct ModuleContext
+    {
+        let culture:SymbolGraph.Culture
+        let symbol:Symbol.Module
+        let id:Unidoc.Scalar
+    }
+}
---
+++ Sources/UnidocLinker/Sema/Unidoc.LinkerTables.ModuleNamespace.swift
@@ -0,0 +1,16 @@
+import Symbols
+import Unidoc
+
+extension Unidoc.LinkerTables
+{
+    struct ModuleNamespace
+    {
+        let culture:Unidoc.Scalar
+        let colony:Unidoc.Scalar
+        let symbol:Symbol.Module
+    }
+}
+extension Unidoc.LinkerTables.ModuleNamespace
+{
+    var cultureOffset:Int { .init(self.culture.citizen) }
+}
