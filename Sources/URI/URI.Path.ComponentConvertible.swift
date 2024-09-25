24639f631cdfe9335af322249daa08763f496bee
Dianna <mdianna1989@gmail.com>
2024-09-25 23:57:07 +0000

    introduce URI.Path.ComponentConvertible

---
+++ Sources/URI/URI.Path.ComponentConvertible.swift
@@ -0,0 +1,17 @@
+import URI
+
+extension URI.Path
+{
+    public
+    protocol ComponentConvertible:LosslessStringConvertible
+    {
+    }
+}
+extension URI.Path.ComponentConvertible where Self:RawRepresentable<String>
+{
+    @inlinable public
+    init?(_ description:String) { self.init(rawValue: description) }
+
+    @inlinable public
+    var description:String { self.rawValue }
+}
