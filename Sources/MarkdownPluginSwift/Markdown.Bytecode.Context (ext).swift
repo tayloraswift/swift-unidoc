eda58af9d60cf24355ee6ca82483dac0c4ffccca
Dianna <mdianna1989@gmail.com>
2024-11-03 21:44:52 +0000

    upgrade swift-syntax and indexstore-db dependencies

--- Sources/MarkdownPluginSwift/Markdown.Bytecode.Context (ext).swift
+++ Sources/MarkdownPluginSwift/Markdown.Bytecode.Context (ext).swift
@@ -7,29 +7,24 @@ extension Markdown.Bytecode.Context
     {
         switch classification
         {
-        case    .none,
-                .editorPlaceholder:     return nil
-
-        case    .attribute:             self = .attribute
-
-        case    .ifConfigDirective:     self = .directive
-
-        case    .lineComment,
-                .blockComment:          self = .comment
-        case    .docLineComment,
-                .docBlockComment:       self = .doccomment
-
-        case    .dollarIdentifier:      self = .pseudo
-        case    .identifier:            self = .identifier
-        case    .operator:              self = .operator
-
-        case    .integerLiteral,
-                .floatLiteral:          self = .literalNumber
-        case    .stringLiteral,
-                .regexLiteral:          self = .literalString
-
-        case    .keyword:               self = .keyword
-        case    .type:                  self = .type
+        case .none:                 return nil
+        case .editorPlaceholder:    return nil
+        case .argumentLabel:        self = .identifier
+        case .attribute:            self = .attribute
+        case .ifConfigDirective:    self = .directive
+        case .lineComment:          self = .comment
+        case .blockComment:         self = .comment
+        case .docLineComment:       self = .doccomment
+        case .docBlockComment:      self = .doccomment
+        case .dollarIdentifier:     self = .pseudo
+        case .identifier:           self = .identifier
+        case .operator:             self = .operator
+        case .integerLiteral:       self = .literalNumber
+        case .floatLiteral:         self = .literalNumber
+        case .stringLiteral:        self = .literalString
+        case .regexLiteral:         self = .literalString
+        case .keyword:              self = .keyword
+        case .type:                 self = .type
         }
     }
 }
