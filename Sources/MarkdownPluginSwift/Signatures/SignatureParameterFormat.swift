f4ebb2c8064c0ac79fa9d3809ec8381db39cdbc4
Diana <diana@rarestype.com>
2024-12-25 19:51:50 +0000

    refactor SignatureSyntax.Encoder into an Encoder, a Builder, and a generic Visitor

--- Sources/MarkdownPluginSwift/Signatures/SignatureParameterFormat.swift
+++
@@ -1,9 +0,0 @@
-import SwiftSyntax
-
-protocol SignatureParameterFormat
-{
-    init(syntax:FunctionParameterSyntax, func:Bool)
-
-    static
-    func += (signature:inout SignatureSyntax.Encoder<Self>, self:Self)
-}
