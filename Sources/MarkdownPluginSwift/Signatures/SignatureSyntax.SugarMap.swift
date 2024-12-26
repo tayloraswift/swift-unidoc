60dc900ce10370bd1159f0039bd9524eb69c3857
Dianna <mdianna1989@gmail.com>
2024-12-26 01:01:20 +0000

    add Autograph tests

--- Sources/MarkdownPluginSwift/Signatures/SignatureSyntax.SugarMap.swift
+++ Sources/MarkdownPluginSwift/Signatures/SignatureSyntax.SugarMap.swift
@@ -4,17 +4,17 @@ extension SignatureSyntax
     struct SugarMap
     {
         @usableFromInline
-        var arrays:Set<Int>
-        @usableFromInline
         var dictionaries:Set<Int>
+        @usableFromInline
+        var arrays:Set<Int>
         @usableFromInline
         var optionals:Set<Int>
 
         @inlinable
         init()
         {
-            self.arrays = []
             self.dictionaries = []
+            self.arrays = []
             self.optionals = []
         }
     }
