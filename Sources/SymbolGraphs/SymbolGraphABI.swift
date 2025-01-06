226ca122f4b26809accc34864bd12e376883fcb3
Diana <diana@rarestype.com>
2025-01-06 23:12:09 +0000

    bump various version numbers

--- Sources/SymbolGraphs/SymbolGraphABI.swift
+++ Sources/SymbolGraphs/SymbolGraphABI.swift
@@ -3,5 +3,5 @@ import SemanticVersions
 @frozen public
 enum SymbolGraphABI
 {
-    @inlinable public static var version:PatchVersion { .v(0, 12, 4) }
+    @inlinable public static var version:PatchVersion { .v(0, 12, 5) }
 }
