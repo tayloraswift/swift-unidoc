fe3f8ffed3a04ccf66f4a69cfd88ff5fbcdea85a
Dianna <mdianna1989@gmail.com>
2025-06-29 00:32:26 +0000

    fix sendablemetatype

--- Sources/UnidocCLI/Regex (ext).swift
+++ Sources/UnidocCLI/Regex (ext).swift
@@ -1,6 +1,6 @@
 import ArgumentParser
 
-extension Regex<Substring>:@retroactive ExpressibleByArgument
+extension Regex<Substring>:@retroactive ExpressibleByArgument, @retroactive _SendableMetatype
 {
     @inlinable public
     init?(argument:String)
