acbc8d823779e96929f26308af28a407e51306a1
Diana Ma <diana@rarestype.com>
2026-03-06 21:05:09 +0000

    _SendableMetatype

--- Sources/UnidocCLI/Regex (ext).swift
+++ Sources/UnidocCLI/Regex (ext).swift
@@ -1,6 +1,6 @@
 import ArgumentParser
 
-extension Regex<Substring>: @retroactive ExpressibleByArgument {
+extension Regex<Substring>: @retroactive ExpressibleByArgument, _SendableMetatype {
     @inlinable public init?(argument: String) {
         do {
             try self.init(argument)
