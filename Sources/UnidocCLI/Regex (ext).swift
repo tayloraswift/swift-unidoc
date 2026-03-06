fad550f0db5da8fe088623aa45420dc1169a7cea
Diana Ma <diana@rarestype.com>
2026-03-06 21:58:01 +0000

    upgrade dependency

--- Sources/UnidocCLI/Regex (ext).swift
+++ Sources/UnidocCLI/Regex (ext).swift
@@ -1,6 +1,6 @@
 import ArgumentParser
 
-extension Regex<Substring>: @retroactive ExpressibleByArgument, _SendableMetatype {
+extension Regex<Substring>: @retroactive ExpressibleByArgument {
     @inlinable public init?(argument: String) {
         do {
             try self.init(argument)
